f2c9703e096cf642e72d573d7f31cbdeb76b6ed1
meads58 <meads109@gmail.com>
2014-01-13 17:12:23 +0000

    initial commit

---
+++ src/main/java/com/seleniumsimplified/webdriver/App.java
@@ -0,0 +1,13 @@
+package com.seleniumsimplified.webdriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
